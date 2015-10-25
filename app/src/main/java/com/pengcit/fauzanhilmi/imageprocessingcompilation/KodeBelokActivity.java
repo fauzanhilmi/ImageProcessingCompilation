5fe2e78541998c2c17c33a73a50e18545fcabedf
fauzanhilmi <fauzanhilmir@gmail.com>
2015-10-25 20:25:22 +0700

    tampilin kode belok (untested)

--- app/src/main/java/com/pengcit/fauzanhilmi/imageprocessingcompilation/KodeBelokActivity.java
+++ app/src/main/java/com/pengcit/fauzanhilmi/imageprocessingcompilation/KodeBelokActivity.java
@@ -1,24 +1,117 @@
 package com.pengcit.fauzanhilmi.imageprocessingcompilation;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pengcit.fauzanhilmi.imageprocessingcompilation.ChainCode_Grid_KodeBelok.KodeBelok.TurnCode;
+import com.pengcit.fauzanhilmi.imageprocessingcompilation.ChainCode_Grid_KodeBelok.PlatNomorDetector.NumberDetection;
+
+import java.util.ArrayList;
 
 
 public class KodeBelokActivity extends ActionBarActivity {
+    Button UButton;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kode_belok);
+        setContentView(R.layout.activity_grid);
+
+
+        UButton = (Button)findViewById(R.id.uploadButton);
+        UButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(),"keklik",Toast.LENGTH_LONG).show();
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i,1);
+            }
+        });
+//        dbButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //Toast.makeText(getApplicationContext(),"keklik",Toast.LENGTH_LONG).show();
+//                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//
+//                startActivityForResult(i,1);
+//            }
+//        });
+        tv = (TextView) findViewById(R.id.tv);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            Log.d("image location", picturePath);
+            cursor.close();
+
+            ImageView canvas = (ImageView) findViewById(R.id.canvas);
+
+            canvas.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            Bitmap b = BitmapFactory.decodeFile(picturePath);
+//            ImageProcessor.init(b);
+//            //ImageProcessor.getChainCode();
+//            int num = ImageProcessor.solveOne();
+//            tv.setText(Integer.toString(num));
+
+            NumberDetection nd = new NumberDetection(b);
+            nd.identifyImage();
+            ArrayList<Character> ai = nd.getResults();
+            String res = "";
+            for(int i=0; i<ai.size(); i++) {
+                Log.d("ai",Integer.toString(ai.get(i)));
+//                res += Integer.toString(ai.get(i));
+                res += ai.get(i);
+                res += ' ';
+            }
+
+            //ArrayList<Integer> kodebelok = TurnCode.processChainCode()
+            res += "KodeBelok : ";
+            for(int i=0; i<nd.pxTracer.areas.size(); i++) {
+                ArrayList<Integer> kodebelok = TurnCode.toTurnCode(TurnCode.processChainCode(nd.pxTracer.areas.get(i).chainCode));
+                res += "[";
+                for(int j=0; j<kodebelok.size(); j++) {
+                    res += Integer.toString(kodebelok.get(j));
+                    res += ", ";
+                }
+                res += "]; ";
+            }
+            Log.d("res",res);
+            tv.setText(res);
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_kode_belok, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
